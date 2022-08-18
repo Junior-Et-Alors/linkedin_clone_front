81b1f32cd372c5993b7c697775adffd9b307abc3
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-08-18 23:02:47 +0200

    :sparkles: feat: simple 404 page + responsive styles

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,15 @@
 /* Error 404 */
+import { Link } from 'react-router-dom';
+import "./../assets/styles/error.scss";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className='error'>
+      <h1 className="errorTitle">404</h1>
+      <p className="errorText">Oups ! La page que vous demandez n'existe pas.</p>
+      <Link to='/' className='errorHomeLink'>Retourner sur la page d'accueil</Link>
+    </div>
+  );
 };
 
 export default Error;
